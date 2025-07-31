536530e0925aa54f83e4593c35d209621f793302
TEK Student <TEKstudent@vlab.lan>
2025-07-31 10:55:52 -0700

    intentionally violate policy

--- main.tf
+++ main.tf
@@ -67,10 +67,27 @@ module "lb_security_group" {
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
+  ingress_rules       = ["ssh-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH open to the world"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]  
+
   tags = {
     project     = "project-alpha",
     environment = "dev"
   }
+}
+
+resource "aws_ebs_volume" "unencrypted" {
+  availability_zone = "us-west-1a"
+  size              = 8
+  encrypted         = false # Intentional violation: unencrypted EBS volume
 }
 
 resource "random_string" "lb_id" {
